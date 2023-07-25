ed1168fe23a45fb997afe660d591246270d18be1
Alberto Mayorga Naranjo <alberto.mayorga@aturnos.com>
2023-07-25 08:55:32 +0200

    Código final de la sección 5

---
+++ src/app/dbz/components/character/character.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.css']
+})
+export class CharacterComponent {
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name:'',
+    power:0
+  };
+
+  emitCharacter():void{
+    console.log(this.character);
+
+    if(this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character = {id:uuid(), name:'', power:0};
+  }
+}
